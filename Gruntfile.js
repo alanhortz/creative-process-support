7c6bbca233c3e0a673f7dff49c548009eeee5aec
Alan Hortz <alan.hortz@gmail.com>
2014-02-14 09:04:43 +0100

    working version of jshint task !

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,9 @@
 module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    jshint: {}
+    jshint: {
+      myFiles: ['app/**/*.js']
+    }
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
